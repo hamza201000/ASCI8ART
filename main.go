d8e770dfd7253d10f40abc81420b2cc40776cadf
hamza201000 <HAMZAGHOMARI700@gmail.com>
2025-07-03 23:27:44 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -19,23 +19,40 @@ func main() {
 	//str:=""
 	b := false
 	for sacnneer.Scan() {
-		if sacnneer.Text() == "" && b {
+		if sacnneer.Text() == "" && !b {
+			continue
+
+		}
+		if sacnneer.Text() == "" {
 			asci = append(asci, asci_line)
 			asci_line = []string{}
+
+		} else {
+
+			asci_line = append(asci_line, sacnneer.Text())
+			b = true
+		}
+
+	}
+
+	str := "Hello\nThere"
+	//fmt.Println(asci[0])
+	b1 := [][]string{}
+	newlne := []string{"\n"}
+	for i := 0; i < len(str); i++ {
+		m := (str[i] - 32)
+		if str[i] == '\n' {
+			b1 = append(b1, newlne)
 			continue
 		}
-		b = true
-		asci_line = append(asci_line, sacnneer.Text())
+		b1 = append(b1, asci[m])
 	}
-	str := "hamza"
-	//fmt.Println(asci[0])
-
-	m := 0
-
+	fmt.Println(b1)
 	for i := 0; i < 8; i++ {
-		for j := 0; j < len(str); j++ {
-			m = int(str[j] - 32)
-			fmt.Print(asci[m][i])
+		for j := 0; j < len(b1); j++ {
+			
+			//m := (str[j] - 32)
+			fmt.Print(b1[j][i])
 		}
 		fmt.Println()
 	}
